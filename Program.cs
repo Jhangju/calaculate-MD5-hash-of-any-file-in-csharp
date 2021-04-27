c2d0889073a148cef788e6997178600e2fa98cc1
Jangju <83189731+white-collor@users.noreply.github.com>
2021-04-27 18:47:28 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace calculate_hash
+{
+    class Program
+    {
+       
+        static string CalculateMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+           
+            string path= System.Reflection.Assembly.GetEntryAssembly().Location;
+            string full="\""+path+"\"";
+            Console.WriteLine(path);
+       
+            Console.WriteLine(CalculateMD5(path));
+            
+            Console.ReadLine();
+           
+        }
+    }
+}
